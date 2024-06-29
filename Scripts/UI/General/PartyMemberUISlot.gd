c5cbe747ee435d1ae91c1d72c5c02eed5200f7f8
EveningComet <165712308+EveningComet@users.noreply.github.com>
2024-06-29 08:46:37 -0400

    UI Elements Setup
    Created some basic, needed UI elements.

---
+++ Scripts/UI/General/PartyMemberUISlot.gd
@@ -0,0 +1,24 @@
+## A collection of UI components that display the vitals of a character to
+## the player. This can be used to show characters in the active party
+## and the reserves.
+class_name PartyMemberUISlot extends PanelContainer
+
+## The current character being kept track of.
+@export var curr_char: Combatant:
+	get: return curr_char
+	set(value):
+		curr_char = value
+		char_name_label.set_text(curr_char.name)
+
+@export var char_name_label: Label
+
+# TODO: Reformat the bars to a reusable UI component.
+@export var hp_bar: ProgressBar
+@export var sp_bar: ProgressBar
+@export var xp_bar: ProgressBar
+
+
+
+func on_stat_changed() -> void:
+	# Update the bar if the health, special points, or experience changed
+	curr_char
---
+++ Scripts/UI/General/PlayerHud.gd
@@ -0,0 +1,7 @@
+## Displays the vitals of the player's current party.
+class_name PlayerHud extends Control
+
+## Stores the visuals of the displayed party.
+@export var container: Container
+
+
